04a86cf8bb8201d89686f8256232a756022762a3
Khanh Nguyen <khanh@mobilefolk.com>
2024-04-15 10:56:02 +0700

    sdet pro - done lab_02

--- src/SdetPro/LAB_02.java
+++ src/SdetPro/LAB_02.java
@@ -41,5 +41,34 @@ Lab 2.1 + Suggest user to increase/decrease weight
         System.out.println("-------------------------------------");
 
         //Lab 2.2
+        System.out.println("Lab 2.2");
+        System.out.print("Input n: ");
+        int n = scanner.nextInt();
+        int sodu = n%2;
+        if(sodu == 0) {
+            System.out.println(n + " is even number");
+        } else{
+            System.out.println(n + " is odd number");
+        }
+        System.out.println("-------------------------------------");
+
+
+        //Lab 2.3
+        System.out.println("Lab 2.3");
+        System.out.print("Input weight(kg): ");
+        double weight23 = scanner.nextDouble();
+        System.out.print("Input height(m): ");
+        double height23 = scanner.nextDouble();
+        double bmi23 = weight/(height * height);
+        if(bmi<18.5){
+            System.out.println(bmi + " :Underweight" + "- You should review your diet or eat more diligently.");
+        } else if (bmi>=18.5 && bmi<=24.9) {
+            System.out.println(bmi + " :Normal weight" + " - You should continue to maintain it.");
+        } else if (bmi>=25 && bmi <= 29.9) {
+            System.out.println(bmi + " :Overweight" + "- You are overweight, adjust your diet and exercise harder.");
+        } else if (bmi>=30) {
+            System.out.println(bmi + " :Obesity" + " If you are in this group, it is quite dangerous. You need to review your nutrition regimen, consult your doctor as well as hire a personal PT to guide you on how to exercise most effectively.");
+        }
+        System.out.println("-------------------------------------");
     }
 }
